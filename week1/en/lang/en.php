33d8748333732c73d44c88fe99aa9670b152a4e4
Mohamed Sayed <109354903+MohamedHbo@users.noreply.github.com>
2025-01-26 00:57:26 +0200

    Add files via upload

---
+++ week1/en/lang/en.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    'title' => 'My Website',
+    'home' => 'Home',
+    'about' => 'About Us',
+    'contact' => 'Contact',
+    'welcome' => 'Welcome to our website!',
+    'name' => 'Name',
+    'email' => 'Email',
+    'message' => 'Message',
+    'submit' => 'Submit',
+];
